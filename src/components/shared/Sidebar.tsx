b5137de1f3bea93ce159a2a8b61b16a122604434
eljudepanao <eljudepanao03@gmail.com>
2024-10-11 01:04:54 +0800

    testing

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -85,7 +85,7 @@ const Sidebar = () => {
 
           <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href="/sign-in">Login</Link>
+              <Link href="/sign-in">Login 1</Link>
             </Button>
             <div>
               <p>
